d859a18da12d2340e030df64fa19853651374260
Matthew Bratrsovsky <realzing@pm.me>
2024-05-22 16:21:33 -0600

    fix: account select change on blur, not on change

--- src/lib/components/AccountSelect.svelte
+++ src/lib/components/AccountSelect.svelte
@@ -3,7 +3,7 @@ import { goto } from "$app/navigation";
 import { deals } from "$lib/stores";
 </script>
 
-<select id="account-select" on:change={(e) => e.target && goto("/payments?id="+e.target.value) } class="bg-surface-800 text">
+<select id="account-select" on:blur={(e) => e.target && goto("/payments?id="+e.target.value) } class="bg-surface-800 text">
   {#each Object.entries($deals) as [key, deal]}
     <option value={deal.account} >{key}</option>
   {/each}
