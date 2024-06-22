5615262422bd69ec9d19a3dbd0ad237ac36f987a
Matthew Bratrsovsky <realzing@pm.me>
2024-06-22 12:02:22 -0600

    fix(s3): allow for prefix when listing objects

--- src/lib/server/s3/listObjects.ts
+++ src/lib/server/s3/listObjects.ts
@@ -5,6 +5,7 @@ import { ListObjectsCommand } from "@aws-sdk/client-s3";
 export const listObjects = async (bucketName: string, prefix: string) => {
 	const command = new ListObjectsCommand({
 		Bucket: bucketName,
+		Prefix: prefix,
 	});
 
 	const result = await s3Client.send(command);
