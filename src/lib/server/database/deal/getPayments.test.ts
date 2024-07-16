9b7ec82a67030cda0b1fc12c360f46d218041872
Matthew Bratrsovsky <realzing@pm.me>
2024-07-16 16:53:22 -0600

    feat(db/payments): get all payments within a given month and year

---
+++ src/lib/server/database/deal/getPayments.test.ts
@@ -0,0 +1,40 @@
+import { describe, expect, it, assert } from "vitest";
+import { getMonthlyPayments } from "./getPayments";
+
+describe("Can get payments within a month and year", () => {
+	const testYear = 2022;
+	const testMonth = 11;
+
+	const dateBegins = new Date(testYear, testMonth, 1);
+	const dateEnds = new Date(testYear, testMonth + 1, 0);
+
+	assert(
+		dateBegins.getFullYear() === testYear &&
+			dateBegins.getMonth() === testMonth,
+		"Invalid start date conversion: " + JSON.stringify(dateBegins),
+	);
+	assert(
+		dateEnds.getFullYear() === testYear && dateEnds.getMonth() === testMonth,
+		"Invalid end date conversion",
+	);
+	const testDate = (date: Date) => {
+		const month = date.getMonth();
+		const year = date.getFullYear();
+		return month === testMonth - 1 && year === testYear;
+	};
+
+	it("fetches payments and expects each payment to fall within date", async () => {
+		const payments = await getMonthlyPayments({
+			year: testYear,
+			month: testMonth,
+		});
+
+		for (const payment of payments) {
+			const date = new Date(payment.date);
+			expect(date).toBeInstanceOf(Date);
+
+			// console.debug("testing", date);
+			expect(testDate(date)).toBeTruthy();
+		}
+	});
+});
--- src/lib/server/database/deal/getPayments.ts
+++ src/lib/server/database/deal/getPayments.ts
@@ -8,3 +8,17 @@ export const getPayments = async (deal: string) => {
 		},
 	});
 };
+
+export const getMonthlyPayments = async ({
+	month,
+	year,
+}: { month: number; year: number }) => {
+	const date = new Date(year, month, 2);
+	return prisma.payment.findMany({
+		where: {
+			date: {
+				startsWith: `${date.getFullYear()}-${date.getMonth()}`,
+			},
+		},
+	});
+};
