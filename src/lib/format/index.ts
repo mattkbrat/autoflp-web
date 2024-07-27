4c2182ffc0c91843754c92b352f6b8a256aefcd0
Matthew Bratrsovsky <realzing@pm.me>
2024-07-27 17:15:51 -0600

    fix(format/currency): remove dollar sign and negative clutter

--- src/lib/format/index.ts
+++ src/lib/format/index.ts
@@ -1,5 +1,3 @@
-import type { Person } from "$lib/server/database/models/Person";
-
 type DealPerson = NonNullable<DetailedDeal>["account"]["contact"];
 
 export type FullNameParams = {
@@ -15,6 +13,7 @@ export { fullNameFromPerson, addressFromPerson };
 
 import { format } from "date-fns";
 import type { DetailedDeal } from "$lib/server/database/deal";
+import type { Person } from "@prisma/client";
 
 const currencyFormat = new Intl.NumberFormat("en-US", {
 	style: "currency",
@@ -32,8 +31,15 @@ export const formatCurrency = (
 ) => {
 	if (!amount || Number.isNaN(Number(amount))) return "-";
 	if (+amount === 0 && blankZeroes) return "-";
-	const formatted = currencyFormat.format(Number(amount || 0));
-	return `${formatted[0]} ${formatted.slice(1)}`;
+	const num = Number(amount || 0);
+	const abs = Math.abs(num);
+	const formatted = currencyFormat.format(abs).slice(1);
+	if (num > 0) {
+		return formatted;
+	}
+	return `( ${formatted} )`;
+	// console.log({ formatted });
+	// return formatted;
 };
 
 export const dateFormatStandard = "MM/dd/yyyy";
