3d57f615b1a4a6f61dd8e64002a136e0144f8e35
Matthew Bratrsovsky <realzing@pm.me>
2024-07-27 17:28:05 -0600

    feat(client/home): quicklinks to open accounts for payments
    TODO: Improve the theme/UX

--- src/routes/(manage)/home/+page.svelte
+++ src/routes/(manage)/home/+page.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
 import { page } from "$app/stores";
+import { formatDate, fullNameFromPerson } from "$lib/format";
+
+import { deals } from "$lib/stores";
 </script>
 
 <h2>
-    {$page.url.pathname}
+  {$page.url.pathname}
 </h2>
+<p>Open Deals</p>
+<div class="flex flex-col">
+  {#each $deals as deal}
+    <a href={`/payments/${deal.account.id}/${deal.id}`}
+      >{fullNameFromPerson({ person: deal.account.contact })}
+      {formatDate(deal.date)}</a
+    >
+  {/each}
+</div>
