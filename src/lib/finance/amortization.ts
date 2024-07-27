7d2d2f7e39eb38596e24ff1c636b4f2a02e2edf3
Matthew Bratrsovsky <realzing@pm.me>
2024-07-27 17:15:28 -0600

    fix(amortization/history): better delinquent balance handling

--- src/lib/finance/amortization.ts
+++ src/lib/finance/amortization.ts
@@ -1,4 +1,11 @@
-import { addMonths, isAfter, isSameMonth } from "date-fns";
+import {
+	addMonths,
+	isAfter,
+	isBefore,
+	isSameMonth,
+	nextMonday,
+	startOfMonth,
+} from "date-fns";
 import { roundToPenny } from "./roundToPenny";
 import type { Deal } from "@prisma/client";
 import { getPercent } from "./getPercent";
@@ -23,48 +30,66 @@ export const amoritization = ({
 	startDate,
 	history,
 }: AmortizationParams) => {
+	const withHistory = !!history;
 	const monthlyRate = apr / term;
 	let lastBalance = balance;
 	let totalDelinquent = 0;
 	let actualTotalDelinquent = 0;
+	let totalPaid = 0;
+	let n = -1;
 	const today = new Date();
 
 	console.log({ monthlyRate, term, apr, balance, pmt });
 
-	const schedule = Array.from(new Array(term).keys()).map((_, n) => {
-		const date = addMonths(startDate, n);
-		const dateAfterToday = !!history && isAfter(date, today);
+	let schedule = [];
+
+	while (lastBalance > 0 && n < 100) {
+		n++;
+
+		const dueDate = addMonths(startDate, n);
+		const dateAfterToday = withHistory && isAfter(dueDate, today);
+		const date = startOfMonth(dueDate);
+
 		const matchingPayments = history?.filter((p) => isSameMonth(date, p.date));
+		const ignore = !dateAfterToday && !matchingPayments?.length;
 		const totalPaidInMonth = matchingPayments?.reduce(
 			(acc, p) => acc + Number(p.amount),
 			0,
 		);
 
-		const pmtDiff = totalDelinquent ? totalDelinquent : 0;
+		if (totalPaidInMonth) {
+			totalPaid += totalPaidInMonth || 0;
+		}
+
+		const pmtDiff = totalDelinquent
+			? Math.min(totalDelinquent, balance - totalPaid)
+			: 0;
 		const schedulePmt = !dateAfterToday ? totalPaidInMonth || 0 : pmt + pmtDiff;
 		if (dateAfterToday) {
 			totalDelinquent = 0;
 		}
 
-		const interest = lastBalance * monthlyRate;
+		const interest = ignore ? 0 : lastBalance * monthlyRate;
 		const delinquentBalance = dateAfterToday
 			? 0
-			: totalPaidInMonth
-				? pmt - totalPaidInMonth
-				: pmt;
+			: pmt - (totalPaidInMonth || 0);
 
-		console.log({
-			schedulePmt,
-			delinquentBalance,
-			date,
-			pmtDiff,
-			totalDelinquent,
-		});
+		if (withHistory) {
+			actualTotalDelinquent += delinquentBalance;
+			totalDelinquent = Math.min(
+				totalDelinquent + delinquentBalance,
+				balance - totalPaid,
+			);
+			console.log({
+				schedulePmt,
+				delinquentBalance,
+				date,
+				pmtDiff,
+				totalDelinquent,
+			});
+		}
 
-		actualTotalDelinquent += delinquentBalance;
-		totalDelinquent += delinquentBalance || 0;
-
-		let principal = roundToPenny(schedulePmt - interest);
+		let principal = ignore ? 0 : roundToPenny(schedulePmt - interest);
 		lastBalance -= principal;
 		if (lastBalance < 1) {
 			principal += lastBalance;
@@ -73,22 +98,32 @@ export const amoritization = ({
 		const percentPrincipal = roundToPenny(principal / (interest + principal));
 		const percentInterest = roundToPenny(1 - percentPrincipal);
 
-		return {
+		const scheduleRow = {
 			lastBalance,
 			principal,
 			interest,
 			date,
 			paid: totalPaidInMonth,
-			expected: totalPaidInMonth ? 0 : schedulePmt || pmt,
+			expected: !dateAfterToday ? pmt : schedulePmt,
 			percentPrincipal:
 				percentPrincipal < 1 && percentPrincipal > 0 ? percentPrincipal : 0,
 			percentInterest:
 				percentInterest < 1 && percentInterest > 0 ? percentInterest : 0,
 			delinquentBalance,
 		};
-	});
 
-	return { schedule, monthlyRate, lastBalance, pmt, totalDelinquent };
+		schedule.push(scheduleRow);
+	}
+
+	// console.log({ totalPaid, actualTotalDelinquent, lastBalance, balance });
+	return {
+		schedule,
+		monthlyRate,
+		lastBalance,
+		pmt,
+		totalDelinquent: actualTotalDelinquent,
+		totalPaid,
+	};
 };
 
 export const dealAmortization = (deal: Deal, payments: Payments) => {
@@ -101,7 +136,7 @@ export const dealAmortization = (deal: Deal, payments: Payments) => {
 		balance,
 		apr,
 		pmt: Number(deal.pmt),
-		startDate: new Date(deal.date),
+		startDate: addMonths(new Date(deal.date), 1),
 		history: payments,
 	};
 
