bdf395c1d61b8da0aaade1df9d7248503c2eef19
Matthew Bratrsovsky <realzing@pm.me>
2024-08-02 16:29:11 -0600

    feat(db): autosales schema

--- src/lib/server/database/index.ts
+++ src/lib/server/database/index.ts
@@ -1,3 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaClient as ComClient } from "@prisma/autosales";
 
 export const prisma = new PrismaClient();
+export const comClient = new ComClient();
