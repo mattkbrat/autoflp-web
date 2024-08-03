43560cff8673f7254e5dda22b62830d2994c1174
Matthew Bratrsovsky <realzing@pm.me>
2024-08-03 11:17:40 -0600

    fix(client): sort inv images by order property

--- src/lib/server/database/com/inventory/index.ts
+++ src/lib/server/database/com/inventory/index.ts
@@ -1,11 +1,21 @@
 import { comClient } from "$lib/server/database";
 import type { Prisma } from "@prisma/client";
+import type { Image } from "@prisma/autosales";
 export const getCurrentInventory = async () => comClient.inventory.findMany({});
 
 export const getSingleInventory = async (id: number) => {
 	return comClient.inventory.findUnique({
 		where: { id },
-		include: { images: true },
+		include: {
+			images: {
+				orderBy: {
+					order: "asc",
+				},
+			},
+		},
+	});
+};
+
 export const updateInventoryImage = async (
 	id: number,
 	data: Partial<Image>,
