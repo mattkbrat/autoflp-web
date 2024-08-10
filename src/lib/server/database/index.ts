0f4d4f85b2d55cb11a82e508f8f6a4b2765424c8
Matthew Bratrsovsky <realzing@pm.me>
2024-08-10 16:14:49 -0600

    fix(deploy): wrap prisma as require for adapter-node
    https://github.com/liloryan/iru_belongings/commit/3044d732133df397e324eaef19b8905b31a6f442

--- src/lib/server/database/index.ts
+++ src/lib/server/database/index.ts
@@ -1,5 +1,8 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaClient as ComClient } from "@prisma/autosales";
+import { createRequire } from "node:module";
+
+const require = createRequire(import.meta.url);
+const { PrismaClient } = require("@prisma/client");
+const { PrismaClient: ComClient } = require("@prisma/autosales");
 
 export const prisma = new PrismaClient();
 export const comClient = new ComClient();
