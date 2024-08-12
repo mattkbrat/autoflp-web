c7053f0e0a5f13d7efa21fbc1104dd84dcfebc70
Matthew Bratrsovsky <realzing@pm.me>
2024-08-12 12:51:49 -0600

    fix(s3): r2 does not honor prefix request, manually filter

--- src/lib/server/s3/listObjects.ts
+++ src/lib/server/s3/listObjects.ts
@@ -3,12 +3,18 @@ import { s3Client } from ".";
 import { ListObjectsCommand } from "@aws-sdk/client-s3";
 
 export const listObjects = async (bucketName: string, prefix: string) => {
+	console.log("listing", { bucketName, prefix });
 	const command = new ListObjectsCommand({
 		Bucket: bucketName,
 		Prefix: prefix,
 	});
 
+	// Cloudflare returns all objects, regardless of prefix.
 	const result = await s3Client.send(command);
 
-	return result.Contents;
+	const { Contents } = result;
+
+	if (!prefix) return Contents || [];
+
+	return Contents?.filter((obj) => obj.Key?.startsWith(`${prefix}/`)) || [];
 };
