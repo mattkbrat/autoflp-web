44ad733b1bc337ee28221ea5e1f8b5fd40b72c2d
Matthew Bratrsovsky <realzing@pm.me>
2024-08-12 16:43:31 -0600

    fix(forms/buyers-guide): correct values

--- src/lib/server/form/builder/BUYERS_GUIDE.ts
+++ src/lib/server/form/builder/BUYERS_GUIDE.ts
@@ -6,14 +6,18 @@ import {
 	PRIMARY_DEALER_NAME,
 } from "$env/static/private";
 import type { DetailedDeal } from "$lib/server/database/deal";
+import type { Inventory } from "@prisma/client";
 import type { BuyersGuideTemplate } from "./maps";
 
-export const fillBuyersGuideData = (deal: NonNullable<DetailedDeal>) => {
+export const fillBuyersGuideData = (
+	deal: NonNullable<DetailedDeal> | Partial<Inventory>,
+) => {
+	const inventory = "inventory" in deal ? deal.inventory : deal;
 	return {
-		"0": deal.inventory.make,
-		"1": deal.inventory.model || "",
-		"2": deal.inventory.make,
-		"3": deal.inventory.vin,
+		"0": inventory.make,
+		"1": inventory.model || "",
+		"2": inventory.year,
+		"3": inventory.vin,
 		"4": BUSINESS_NAME,
 		"5": BUSINESS_ADDRESS,
 		"6": PHONE_NUMBER,
