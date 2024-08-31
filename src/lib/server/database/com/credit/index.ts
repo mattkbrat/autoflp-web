c9febd3dcb5169eb06c644278c4092b15550718c
Matthew Bratrsovsky <realzing@pm.me>
2024-08-31 16:46:14 -0600

    feat(credit-app): get app by id & presign associated s3 object images

--- src/lib/server/database/com/credit/index.ts
+++ src/lib/server/database/com/credit/index.ts
@@ -1,4 +1,8 @@
+import { getPresignedUrl } from "$lib/server/s3";
+import type { CreditApplicationWithData } from "$lib/types";
 import { comClient } from "../..";
+import type { CreditApplication } from "@prisma/autosales";
+
 export const getApplications = async () => {
 	return comClient.creditApplication.findMany({
 		select: {
@@ -15,4 +19,53 @@ export const getApplications = async () => {
 			timestamp: "desc",
 		},
 	});
+};
+
+export const getApplication = async ({
+	id,
+}: { id: CreditApplication["id"] }) => {
+	return comClient.creditApplication.findUnique({
+		where: { id },
+		include: {
+			users: {
+				select: {
+					email: true,
+					name: true,
+					userImage: {
+						select: {
+							imageUserImageImageToimage: true,
+						},
+					},
+				},
+			},
+		},
+	});
+};
+
+export const getPresignedImages = async (
+	application: CreditApplicationWithData,
+) => {
+	const images =
+		application?.users.userImage.map((ui) => {
+			const { source, url, title } = ui.imageUserImageImageToimage;
+			const bucket = source.startsWith("r2") ? source.split("r2/")[1] : null;
+
+			return {
+				url,
+				bucket,
+				title,
+			};
+		}) || [];
+
+	if (images.length > 0) {
+		for (let n = 0; n < images.length; n++) {
+			const { url, bucket } = images[n];
+			if (!bucket) continue;
+
+			const href = await getPresignedUrl(bucket, url);
+			images[n].url = href;
+		}
+	}
+
+	return images;
 };
