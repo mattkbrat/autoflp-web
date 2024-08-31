86ab469a8eccf6c1528f72fce5378c2bad9789c2
Matthew Bratrsovsky <realzing@pm.me>
2024-08-31 16:47:41 -0600

    feat(credit-app): display individual credit app
    with a focus on printability

---
+++ src/routes/(manage)/credit/[selected]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+import type { PageData } from "./$types";
+import CreditApplicationViewer from "./CreditApplicationViewer.svelte";
+
+export let data: PageData;
+
+const { images, application } = data;
+</script>
+
+<CreditApplicationViewer credit={application || {}} {images} />
