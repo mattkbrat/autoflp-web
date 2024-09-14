574acc5360f6eab0b575f64cd4c1a4e8f3de1134
Matthew Bratrsovsky <realzing@pm.me>
2024-09-14 13:54:31 -0600

    fix(client): fix formatting for home page

--- src/routes/(manage)/home/+page.svelte
+++ src/routes/(manage)/home/+page.svelte
@@ -1,19 +1,5 @@
 <script lang="ts">
-import { page } from "$app/stores";
-import { formatDate, fullNameFromPerson } from "$lib/format";
-
-import { deals } from "$lib/stores";
+import AccountCombobox from "$lib/components/AccountCombobox.svelte";
 </script>
 
-<h2>
-  {$page.url.pathname}
-</h2>
-<p>Open Deals</p>
-<div class="flex flex-col">
-  {#each $deals as deal}
-    <a href={`/payments/${deal.account.id}/${deal.id}`}
-      >{fullNameFromPerson({ person: deal.account.contact })}
-      {formatDate(deal.date)}</a
-    >
-  {/each}
-</div>
+<AccountCombobox />
