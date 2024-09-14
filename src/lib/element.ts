5b8880f1b152cbacb9c92ed35e26ad76941a1406
Matthew Bratrsovsky <realzing@pm.me>
2024-09-14 13:54:12 -0600

    feat(client): wait for element function

--- src/lib/element.ts
+++ src/lib/element.ts
@@ -1,3 +1,5 @@
+import { browser } from "$app/environment";
+
 export const getElement = <T>(elementId: string) =>
 	document.getElementById(elementId) as T;
 
@@ -19,3 +21,29 @@ export const selectAll = (inputGroup: string) => {
 		c.checked = someUnchecked;
 	}
 };
+
+export function waitForElm<Element extends HTMLElement>(
+	selector: string,
+): Promise<Element | null> {
+	return new Promise((resolve) => {
+		if (!browser) return null;
+		const querySelector = document.querySelector(selector);
+		if (querySelector) {
+			return resolve(querySelector);
+		}
+
+		const observer = new MutationObserver((_mutations) => {
+			const querySelector = document.querySelector(selector);
+			if (querySelector) {
+				observer.disconnect();
+				resolve(querySelector);
+			}
+		});
+
+		// If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+		observer.observe(document.body, {
+			childList: true,
+			subtree: true,
+		});
+	});
+}
