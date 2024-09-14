58b130ee52cb389d7c695a52b74b5f83b4999703
Matthew Bratrsovsky <realzing@pm.me>
2024-09-14 13:53:26 -0600

    refactor(client): move account deal tabs to custom menu
    fixes an issue with overflow on many items

--- src/lib/components/AccountDealTabs.svelte
+++ src/lib/components/AccountDealTabs.svelte
@@ -5,24 +5,27 @@ import { TabAnchor, TabGroup } from "@skeletonlabs/skeleton";
 import { accountDeals } from "$lib/stores";
 </script>
 
-<TabGroup
-  justify="justify-center"
-  active="variant-filled-primary"
-  hover="hover:variant-soft-primary"
-  flex="flex-1 lg:flex-none"
-  rounded=""
-  border=""
-  class="w-full bg-surface-100-800-token"
+<menu
+  class="flex flex-row flex-wrap text-center bg-surface-700 print:hidden self-center"
 >
   {#each $accountDeals || [] as deal}
-    <TabAnchor
+    {@const isSelected = $page.url.pathname.endsWith(deal.id)}
+    <a
       href={`/payments/${deal.accountId}/${deal.id}`}
-      selected={$page.url.pathname.endsWith(deal.id)}
+      class="p-4 min-w-40"
+      class:hover:bg-surface-600={!isSelected}
+      class:bg-primary-600={isSelected}
     >
-      <svelte:fragment slot="lead"
-        >{deal.inventory.make} {deal.inventory.model}</svelte:fragment
-      >
-      <span>{deal.date}</span>
-    </TabAnchor>
+      <li>
+        <span>
+          {deal.inventory.make}
+        </span>
+        <span>
+          {deal.inventory.model}
+        </span>
+        <br />
+        <span>{deal.date.split("T")[0].split(" ")[0]}</span>
+      </li>
+    </a>
   {/each}
-</TabGroup>
+</menu>
