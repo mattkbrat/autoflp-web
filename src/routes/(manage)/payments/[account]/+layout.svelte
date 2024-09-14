57cfcd3f42e63fee80b405fe5777020857e000c0
Matthew Bratrsovsky <realzing@pm.me>
2024-09-14 13:58:48 -0600

    fix(client): do not go to first deal on load
    the new account combo-box directly navigates to the correct route

--- src/routes/(manage)/payments/[account]/+layout.svelte
+++ src/routes/(manage)/payments/[account]/+layout.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
 import { browser } from "$app/environment";
 import { goto } from "$app/navigation";
+import { page } from "$app/stores";
 import AccountDealTabs from "$lib/components/AccountDealTabs.svelte";
 import { accountDeals } from "$lib/stores";
 import type { PageData } from "./$types";
 
 export let data: PageData;
 
-$: if (data.accountDeals) {
+$: if (data.accountDeals && $page.url) {
 	accountDeals.set(data.accountDeals || []);
-	const firstDeal = data.accountDeals[0];
-	if (firstDeal && browser) {
-		goto(`/payments/${firstDeal.accountId}/${firstDeal.id}`);
-	}
 }
 </script>
 
