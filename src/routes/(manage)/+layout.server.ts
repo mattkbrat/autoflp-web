eaa83e9281a01160fd03c4a838ba6a863dab41da
Matthew Bratrsovsky <realzing@pm.me>
2024-09-22 17:43:12 -0600

    feat(client): display business footer on print

--- src/routes/(manage)/+layout.server.ts
+++ src/routes/(manage)/+layout.server.ts
@@ -1,5 +1,10 @@
+import {
+	BUSINESS_ADDRESS,
+	BUSINESS_MOTTO,
+	BUSINESS_NAME,
+	PHONE_NUMBER,
+} from "$env/static/private";
 import { getAccounts } from "$lib/server/database/account";
-import { getAndGroupDeals } from "$lib/server/database/deal";
 import { getDeals } from "$lib/server/database/deal/getDeals";
 import { getInventory } from "$lib/server/database/inventory";
 import type { LayoutServerLoad } from "./$types";
@@ -9,9 +14,17 @@ export const load: LayoutServerLoad = async ({ url }) => {
 	const inventory = await getInventory(1);
 	const accounts = await getAccounts();
 
+	const businessData = {
+		name: BUSINESS_NAME,
+		motto: BUSINESS_MOTTO,
+		phone: PHONE_NUMBER,
+		address: BUSINESS_ADDRESS,
+	};
+
 	return {
 		deals,
 		inventory,
 		accounts,
+		businessData,
 	};
 };
--- src/routes/(manage)/+layout.svelte
+++ src/routes/(manage)/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 import { allAccounts, allInventory, deals } from "$lib/stores";
 import { onMount } from "svelte";
-export let data;
+import type { LayoutData } from "./$types";
+export let data: LayoutData;
 onMount(() => {
 	deals.set(data.deals);
 	allInventory.set(data.inventory);
@@ -10,3 +11,22 @@ onMount(() => {
 </script>
 
 <slot />
+
+<section class="invisible print:visible">
+  <div
+    class="flex flex-col border-t-2 border-black w-full justify-between items-center"
+  >
+    <span class="flex flex-row py-2">
+      <span class="flex-1">
+        {data.businessData.name}
+      </span>
+      <span class="border-l-2 border-black mx-2 h-4 my-auto"> </span>
+      <span>
+        {data.businessData.phone}
+      </span>
+    </span>
+    <span>
+      {data.businessData.address}
+    </span>
+  </div>
+</section>
