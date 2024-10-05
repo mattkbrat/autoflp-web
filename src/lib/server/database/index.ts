5940b24b1a634eb6e05f52a2e6f4c2bf3e5dcbf6
Matthew Bratrsovsky <realzing@pm.me>
2024-10-05 08:58:27 -0600

    fix(db): use require for prod, es6 for dev

--- src/lib/server/database/index.ts
+++ src/lib/server/database/index.ts
@@ -1,7 +1,24 @@
 import { createRequire } from "node:module";
 
+const require = createRequire(import.meta.url);
 import { PrismaClient } from "@prisma/client";
 import { PrismaClient as ComClient } from "@prisma/autosales";
+import { dev } from "$app/environment";
 
-export const prisma = new PrismaClient();
-export const comClient = new ComClient();
+const getClient = () => {
+	return new PrismaClient();
+};
+
+const getComClient = () => {
+	return new ComClient();
+};
+
+const { PrismaClient: RequireClient } = require("@prisma/client");
+const { PrismaClient: RequireComClient } = require("@prisma/autosales");
+
+export const prisma: ReturnType<typeof getClient> = new (
+	dev ? PrismaClient : RequireClient
+)();
+export const comClient: ReturnType<typeof getComClient> = new (
+	dev ? RequireComClient : ComClient
+)();
