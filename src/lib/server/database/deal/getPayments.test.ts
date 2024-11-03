b7392997a3f1ba6260047edf2966738dd8c9c642
Matthew Bratrsovsky <realzing@pm.me>
2024-11-03 16:03:32 -0700

    fix(tests): find an existsing deal with matching criteria for getPayments

--- src/lib/server/database/deal/getPayments.test.ts
+++ src/lib/server/database/deal/getPayments.test.ts
@@ -1,12 +1,17 @@
 import { describe, expect, it, assert } from "vitest";
 import { getMonthlyPayments, sumMonthlyPayments } from "./getPayments";
+import { env } from "$env/dynamic/private";
+import { prisma } from "..";
 
 describe("Can get payments within a month and year", () => {
-	const testYear = 2022;
-	const testMonth = 11;
+	const testYear = 2024;
+	const testMonth = 10;
 
 	const dateBegins = new Date(testYear, testMonth, 1);
 	const dateEnds = new Date(testYear, testMonth + 1, 0);
+	const dealId = env.TEST_DEAL_ID;
+
+	expect(dealId?.length).toBeGreaterThan(0);
 
 	assert(
 		dateBegins.getFullYear() === testYear &&
@@ -39,13 +44,38 @@ describe("Can get payments within a month and year", () => {
 	});
 
 	it("fetches and sums payments made by a single account within a month", async () => {
-		const dealId = "af3dc147-cea2-46cd-a25e-3e83c1236a68";
+		const gte = new Date(testYear, 9, 1).toISOString().split("T")[0];
+		const lte = new Date(testYear, 9, 30).toISOString().split("T")[0];
+		const matchingDeal = await prisma.payment.findFirst({
+			where: {
+				AND: [
+					{
+						date: {
+							gte,
+						},
+					},
+					{
+						date: {
+							lte,
+						},
+					},
+				],
+			},
+			select: {
+				deal: {
+					select: {
+						id: true,
+					},
+				},
+			},
+		});
 		const payments = await sumMonthlyPayments({
 			year: testYear,
 			month: testMonth,
-			dealId,
+			dealId: matchingDeal?.deal.id,
 		});
-		expect(Object.keys(payments).length).toEqual(1);
+		console.log({ payments });
+		expect(Object.keys(payments).length).toBeGreaterThanOrEqual(1);
 		expect(payments[dealId]).not.toBeNaN();
 	});
 });
