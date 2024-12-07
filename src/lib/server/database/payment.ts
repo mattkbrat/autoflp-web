7d0c4d3059ef865981992e4d412a4572688dc2c0
Matthew Bratrsovsky <realzing@pm.me>
2024-12-07 09:53:38 -0700

    feat(payments): if duplicate date and payment are recorded, update value
    Fixes an issue with being able to record due to key constraints.

--- src/lib/server/database/payment.ts
+++ src/lib/server/database/payment.ts
@@ -1,18 +1,51 @@
 import type { Payment } from "@prisma/client";
 import { prisma } from ".";
 import { sendPaymentNotification } from "../notify";
+import { formatCurrency } from "$lib/format";
 
 export const getPaymentsByDeal = async (deal: string) => {
 	return prisma.payment.findMany({ where: { dealId: deal } });
 };
 
+const notify = async (dealId: string, amount: string) => {
+	return sendPaymentNotification({
+		dealId,
+		type: "create",
+		amount: amount,
+	});
+};
+
 export const recordPayment = async (payment: Payment) => {
+	const exists = await prisma.payment.findUnique({
+		where: {
+			dealId_date_amount: {
+				dealId: payment.dealId,
+				date: payment.date,
+				amount: payment.amount,
+			},
+		},
+	});
+
+	if (exists) {
+		return prisma.payment
+			.update({
+				where: {
+					id: exists.id,
+				},
+				data: {
+					amount: formatCurrency(
+						Number(exists.amount) + Number(payment.amount),
+					),
+				},
+			})
+			.then((p) => {
+				notify(p.dealId, p.amount);
+				return p;
+			});
+	}
 	return prisma.payment.create({ data: payment }).then((p) => {
-		sendPaymentNotification({
-			dealId: payment.dealId,
-			type: "create",
-			amount: p.amount,
-		});
+		notify(p.dealId, p.amount);
+
 		return p;
 	});
 };
