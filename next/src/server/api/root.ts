20eade4e0e0bfcf783a2b0d18a00aed9d010f858
Matthew Bratrsovsky <realzing@pm.me>
2025-04-13 17:21:20 -0600

    fix: remove unused template code, format

--- next/src/server/api/root.ts
+++ next/src/server/api/root.ts
@@ -1,5 +1,6 @@
-import { postRouter } from "~/server/api/routers/post";
+// import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { greetingRouter } from "./routers/greeting";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +8,9 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+	// post: postRouter,
+
+	greeting: greetingRouter,
 });
 
 // export type definition of API
