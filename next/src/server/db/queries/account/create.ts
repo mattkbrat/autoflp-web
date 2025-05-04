c8e50e8f408c1779dc1fb2030bade757214c97e8
Matthew Bratrsovsky <realzing@pm.me>
2025-05-03 18:35:28 -0600

    improve(db): license expiration musst be after current date

--- next/src/server/db/queries/account/create.ts
+++ next/src/server/db/queries/account/create.ts
@@ -12,68 +12,77 @@ export const accountSchema = createInsertSchema(account);
 export type AccountSchema = z.infer<typeof accountSchema>;
 export type PersonSchema = z.infer<typeof personSchema>;
 
+
 export const newAccount = z.object({
-	account: accountSchema,
-	person: personSchema,
+  account: accountSchema.refine((s) => {
+    if (!s.licenseExpiration) return false
+    const exp = new Date(s.licenseExpiration);
+    if (Number.isNaN(exp)) return false;
+    return exp.getTime() > new Date().getTime()
+  }, {
+    message: "License expiration must be after the current date",
+    path: ['licenseExpiration']
+  }),
+  person: personSchema,
 });
 
 export type NewAccount = z.infer<typeof newAccount>;
 
 export const upsertAccount = async (o: NewAccount) =>
-	db.transaction(async (tx) => {
-		const exists = await tx.query.person.findFirst({
-			with: {
-				accounts: {
-					columns: {
-						id: true,
-					},
-				},
-			},
-			columns: {
-				id: true,
-			},
-			where: (person, { eq, and, or }) =>
-				or(
-					eq(person.id, o.person.id),
-					and(
-						eq(person.lastName, o.person.lastName),
-						eq(person.firstName, o.person.firstName),
-						eq(person.address1, o.person.address1),
-					),
-				),
-		});
+  db.transaction(async (tx) => {
+    const exists = await tx.query.person.findFirst({
+      with: {
+        accounts: {
+          columns: {
+            id: true,
+          },
+        },
+      },
+      columns: {
+        id: true,
+      },
+      where: (person, { eq, and, or }) =>
+        or(
+          eq(person.id, o.person.id),
+          and(
+            eq(person.lastName, o.person.lastName),
+            eq(person.firstName, o.person.firstName),
+            eq(person.address1, o.person.address1),
+          ),
+        ),
+    });
 
-		{
-			const accountId = exists?.accounts[0]?.id;
-			const personId = exists?.id;
-			console.log("exists", exists, { accountId, personId });
-			if (accountId && personId) {
-				assert(o.person.id, "Missing person id");
-				assert(o.account.id, "Missing account id");
-				assert(o.account.contact, "Missing account id");
+    {
+      const accountId = exists?.accounts[0]?.id;
+      const personId = exists?.id;
+      console.log("exists", exists, { accountId, personId });
+      if (accountId && personId) {
+        assert(o.person.id, "Missing person id");
+        assert(o.account.id, "Missing account id");
+        assert(o.account.contact, "Missing account id");
 
-				await tx.update(person).set(o.person).where(eq(person.id, personId));
-				await tx
-					.update(account)
-					.set(o.account)
-					.where(eq(account.id, accountId));
-				return { person: personId, account: accountId };
-			}
-		}
+        await tx.update(person).set(o.person).where(eq(person.id, personId));
+        await tx
+          .update(account)
+          .set(o.account)
+          .where(eq(account.id, accountId));
+        return { person: personId, account: accountId };
+      }
+    }
 
-		o.person.id = o.person.id || randomUUID();
-		o.account.id = o.account.id || randomUUID();
-		const [newPerson] = await tx
-			.insert(person)
-			.values(o.person)
-			.returning({ id: person.id, lastName: person.lastName });
-		if (!newPerson?.id) throw new Error("Failed to create a contact");
-		const [newAccount] = await tx
-			.insert(account)
-			.values(Object.assign(o.account, { contact: newPerson.id }))
-			.returning({ id: account.id });
+    o.person.id = o.person.id || randomUUID();
+    o.account.id = o.account.id || randomUUID();
+    const [newPerson] = await tx
+      .insert(person)
+      .values(o.person)
+      .returning({ id: person.id, lastName: person.lastName });
+    if (!newPerson?.id) throw new Error("Failed to create a contact");
+    const [newAccount] = await tx
+      .insert(account)
+      .values(Object.assign(o.account, { contact: newPerson.id }))
+      .returning({ id: account.id });
 
-		if (!newAccount?.id) throw new Error("Failed to create account");
+    if (!newAccount?.id) throw new Error("Failed to create account");
 
-		return { person: newPerson, account: o.account.id };
-	});
+    return { person: newPerson, account: o.account.id };
+  });
